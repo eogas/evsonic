fe475a4530359fa3594bdb070934dd9a0c294be0
unknown <evanogas@gmail.com>
2014-03-12 23:28:17 -0500

    add login route

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ module.exports = function(app) {
 	// export all of the routes
 	exports.home = require('./home.js')(app);
 	exports.home = require('./login.js')(app);
+	exports.home = require('./logout.js')(app);
 	exports.home = require('./register.js')(app);
 };
