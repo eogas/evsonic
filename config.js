a0fdd8767e77ca888cf96cf84d625c0c0eb3a7ae
Ryan <ryangreuel@gmail.com>
2014-03-24 22:43:51 -0500

    add new default_config options to config.json

--- config.js
+++ config.js
@@ -3,16 +3,29 @@ var fs = require('fs'),
     defaultConfigPath = './default_config.json',
     configPath = './config.json',
     configRaw,
-    config;
+    config,
+    changed = false;
 
 try {
+    configRaw = fs.readFileSync(defaultConfigPath);
+    defaultConfig = JSON.parse(configRaw);
     configRaw = fs.readFileSync(configPath);
     config = JSON.parse(configRaw);
+    // check if new options have been added to default_config
+    for (var prop in defaultConfig) {
+        if (!config.hasOwnProperty(prop)) {
+            changed = true;
+            config[prop] = defaultConfig[prop];
+        }
+    }
+     // create new config file if changes found
+     if (changed) {
+        fs.writeFile(configPath, JSON.stringify(config, null, 4));
+    }
 } catch (err) {
     // config file doesn't exist, create a new config file
     // with default settings from default_config.json
-    configRaw = fs.readFileSync(defaultConfigPath);
-    config = JSON.parse(configRaw);
+    config = defaultConfig;
     fs.writeFile(configPath, JSON.stringify(config, null, 4));
 } finally {
     module.exports = config;
